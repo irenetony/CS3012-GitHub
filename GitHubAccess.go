b1202837388c78f4a09314dbccb44bcd43059da1
Irene Ann Tony <tonyi@tcd.ie>
2018-11-06 17:23:56 +0000

    Started implementing using Go's github library

---
+++ GitHubAccess.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+
+	"github.com/google/go-github/github"
+
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: "... your access token ..."},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// list all repositories for the authenticated user
+	repos, _, err := client.Repositories.List(ctx, "", nil)
+	if err != nil {
+		return -1
+	}
+}
