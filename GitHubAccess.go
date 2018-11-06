0083e6b31a9bd10d2cb78e1e55ceff0e99aa3d1d
Irene Tony <tonyi@tcd.ie>
2018-11-06 17:58:13 +0000

    Successfully accessed the list of repos of the logged in user.

--- GitHubAccess.go
+++ GitHubAccess.go
@@ -2,24 +2,32 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/google/go-github/github"
-
 	"golang.org/x/oauth2"
 )
 
-func main() {
+//FetchOrganizations gets all the organisations that the user speccified is a member of.
+func FetchOrganizations(username string) ([]*github.Repository, error) {
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: "... your access token ..."},
+		&oauth2.Token{AccessToken: "token"},
 	)
 	tc := oauth2.NewClient(ctx, ts)
-
 	client := github.NewClient(tc)
+	//orgs, _, err := client.Organizations.List(context.Background(), username, nil)
+	repos, _, err := client.Repositories.List(ctx, username, nil)
+	return repos, err
+}
+func main() {
 
-	// list all repositories for the authenticated user
-	repos, _, err := client.Repositories.List(ctx, "", nil)
+	organizations, err := FetchOrganizations("irenetony")
 	if err != nil {
-		return -1
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	for i, organization := range organizations {
+		fmt.Printf("%v. %v\n", i+1, organization.GetName())
 	}
 }
