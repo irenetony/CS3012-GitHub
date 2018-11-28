f5a07064db670c1541a589e51e957e4b62f71bca
Irene Tony <tonyi@tcd.ie>
2018-11-28 19:30:20 +0000

    Website is running and JSON data can be accessed from the HTML file

--- GitHubAccess.go
+++ GitHubAccess.go
@@ -3,38 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/go-github/github"
-	"github.com/thedevsaddam/renderer"
 	"golang.org/x/oauth2"
 )
-
-var rnd *renderer.Render
-
-func init() {
-	opts := renderer.Options{
-		ParseGlobPattern: "tpl/*.html",
-	}
-
-	rnd = renderer.New(opts)
-}
-
-func website(w http.ResponseWriter, r *http.Request) {
-	usr := struct {
-		Name string
-		ID   int
-	}{
-		Name: "John",
-		ID:   001,
-	}
-	err := rnd.HTML(w, http.StatusOK, "website", usr)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 //FetchRepo gets all the organisations that the user speccified is a member of.
 func FetchRepo(username string) (int, error) {
@@ -61,14 +35,10 @@ func FetchFollowing(username string) ([]*github.User, error) {
 	client := github.NewClient(tc)
 	//s := ""
 	users, _, err := client.Users.ListFollowing(ctx, username, nil)
-	for i := 0; i < len(users); i++ {
-		fmt.Printf("%v", users[i].GetLogin())
-	}
-
 	return users, err
 }
 func main() {
-	mux := http.NewServeMux()
+
 	followingUsers, err := FetchFollowing("irenetony")
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
@@ -86,11 +56,12 @@ func main() {
 		}
 	}
 
-	// Call html page handler.
-	mux.HandleFunc("/", website)
-
 	r := gin.Default()
 
+	r.LoadHTMLFiles("tpl/website.html")
+	r.GET("/web", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "website.html", gin.H{"title": "Main website"})
+	})
 	r.GET("/dataJSON", func(c *gin.Context) {
 
 		var msg struct {
@@ -105,9 +76,8 @@ func main() {
 		c.JSON(http.StatusOK, msg)
 	})
 	// Open port for web server.
-	r.Run(":9000")
-	port := ":8000"
-	log.Println("Listening on port", port)
-	http.ListenAndServe(port, mux)
+	// languages the repos are in
+	port := ":9000"
+	r.Run(port)
 
 }
